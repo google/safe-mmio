f0808421f84cadfe4270c11e64251737a077cef0
Andrew Walbran <qwandor@google.com>
2025-03-06 09:20:31 +0000

    Derive Default for wrapper structs.

--- src/fields.rs
+++ src/fields.rs
@@ -8,28 +8,28 @@ use zerocopy::{FromBytes, Immutable, IntoBytes, KnownLayout};
 
 /// Wrapper for a field which may safely be read but not written. Reading may cause side-effects,
 /// changing the state of the device in some way.
-#[derive(Clone, Debug, Eq, FromBytes, Immutable, IntoBytes, KnownLayout, PartialEq)]
+#[derive(Clone, Debug, Default, Eq, FromBytes, Immutable, IntoBytes, KnownLayout, PartialEq)]
 #[repr(transparent)]
 pub struct ReadOnly<T>(pub T);
 
 /// Wrapper for a field which may safely be read with no side-effects but not written.
-#[derive(Clone, Debug, Eq, FromBytes, Immutable, IntoBytes, KnownLayout, PartialEq)]
+#[derive(Clone, Debug, Default, Eq, FromBytes, Immutable, IntoBytes, KnownLayout, PartialEq)]
 #[repr(transparent)]
 pub struct ReadPure<T>(pub T);
 
 /// Wrapper for a field which may safely be written but not read.
-#[derive(Clone, Debug, Eq, FromBytes, Immutable, IntoBytes, KnownLayout, PartialEq)]
+#[derive(Clone, Debug, Default, Eq, FromBytes, Immutable, IntoBytes, KnownLayout, PartialEq)]
 #[repr(transparent)]
 pub struct WriteOnly<T>(pub T);
 
 /// Wrapper for a field which may safely be written and read. Reading may cause side-effects,
 /// changing the state of the device in some way.
-#[derive(Clone, Debug, Eq, FromBytes, Immutable, IntoBytes, KnownLayout, PartialEq)]
+#[derive(Clone, Debug, Default, Eq, FromBytes, Immutable, IntoBytes, KnownLayout, PartialEq)]
 #[repr(transparent)]
 pub struct ReadWrite<T>(pub T);
 
 /// Wrapper for a field which may safely be written (with side-effects) and read with no
 /// side-effects.
-#[derive(Clone, Debug, Eq, FromBytes, Immutable, IntoBytes, PartialEq)]
+#[derive(Clone, Debug, Default, Eq, FromBytes, Immutable, IntoBytes, PartialEq)]
 #[repr(transparent)]
 pub struct ReadPureWrite<T>(pub T);
